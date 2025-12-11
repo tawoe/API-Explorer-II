10d0e2997b22ec8033138b46388a9943fe0152f3
tawoe <tawoe@posteo.de>
2025-12-11 16:09:23 +0100

    Revert "trying compiler options CommonJS"
    This reverts commit 23a1615dcb1825846194097a9146d48e58839ed1.

--- server/app.ts
+++ server/app.ts
@@ -34,11 +34,13 @@ import express, { Application } from 'express'
 import { useExpressServer, useContainer } from 'routing-controllers'
 import { Container } from 'typedi'
 import path from 'path'
+import { fileURLToPath } from 'url'
 import { execSync } from 'child_process'
 import { OAuth2Service } from './services/OAuth2Service'
 
-// __dirname is available in CommonJS
-// const __dirname is automatically available
+// Fix __dirname for ESM/tsx compatibility
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const port = 8085
 const app: Application = express()
